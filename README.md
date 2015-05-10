259697dfde45bfe28f2d8a39504f708a4c9eeeb5
Vincent AUBERT <vincentaubert88@gmail.com>
2015-05-10 20:10:56 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a base to create console applications.
+
+It's base on the Symfony console component and Box to generate phar files automatically.
---
+++ src/VA/Command/HelloCommand.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace VA\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+class HelloCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('hello')
+            ->setDescription('Says hello world')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Hello World');
+    }
+}
